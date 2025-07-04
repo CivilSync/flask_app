bba2a6a2b935e6397fa089599656431a61fa7c37
I589142 <tfuhge02@gmail.com>
2025-07-04 07:39:51 +0200

    version 1.0

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+VERSION = "1.0"
+
+@app.route("/")
+def hello():
+    return f"Hello, World! (version {VERSION})"
+
+if __name__ == "__main__":
+    app.run(debug=True)
